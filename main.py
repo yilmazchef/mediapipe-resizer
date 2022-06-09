0cd4f005cd74b6b5cdb386bfff45f298a2c05ced
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-09 22:15:32 +0200

    image resource is corrected.

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ while True:
     img1 = cv2.imread(IMAGE_PATH)
 
     if len(hands) == 2:
-        # print(detector.fingersUp(hands[0]), detector.fingersUp(hands[1]))
         if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and \
                 detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
             # print("Zoom Gesture")
@@ -32,12 +31,10 @@ while True:
             lmList2 = hands[1]["lmList"]
             # point 8 is the tip of the index finger
             if startDist is None:
-                # length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
                 length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
 
                 startDist = length
 
-            # length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
             length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
 
             scale = int((length - startDist) // 2)
